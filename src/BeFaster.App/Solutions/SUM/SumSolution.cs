ab07c177d484675a3f0966f3239a2c256cb4a84d
sbx_user1051 <sbx_user1051@169.254.20.237>
2024-11-19 21:36:20 +0000

    Tue Nov 19 21:36:20 UTC 2024

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -6,7 +6,13 @@ namespace BeFaster.App.Solutions.SUM
     {
         public static int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            if(x < 0 || x > 100 || y < 0 || y > 100)
+            {
+                throw new ArgumentOutOfRangeException("Inputs must be between 0 and 100.");
+            }
+
+            return x + y;
         }
     }
 }
+
