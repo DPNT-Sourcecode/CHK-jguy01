b49a5d641efa940a262527cae8554bb37845eb62
sbx_user1051 <sbx_user1051@169.254.136.157>
2024-11-20 11:42:15 +0000

    Wed Nov 20 11:42:15 UTC 2024

--- src/BeFaster.App.Tests/Solutions/CHK/CheckOutSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckOutSolutionTest.cs
@@ -16,11 +16,12 @@ namespace BeFaster.App.Tests.Solutions.CHK
         [TestCase("SSSSSTTTT", ExpectedResult = 90)]
         [TestCase("KK", ExpectedResult = 120)]
         [TestCase("KKK", ExpectedResult = 190)]
-        [TestCase("KKKK", ExpectedResult = 190)]
+        [TestCase("KKKK", ExpectedResult = 240)]
         public int ComputePrice_ReturnsCorrectTotal(string skus)
         {
             return CheckoutSolution.ComputePrice(skus);
         }
     }
 }
+
 
